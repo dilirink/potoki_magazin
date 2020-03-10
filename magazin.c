c0a6bce1f34be183dd2de3152c38dc96ec8f89e5
alexey nikylin <dilirink@gmail.com>
2020-03-10 20:42:17 +0700

    WORK!

--- magazin.c
+++ magazin.c
@@ -10,88 +10,218 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #define rand_pok (750+rand()%(1000-750+1)) //генерит для погрузчика  750 до 1000
 #define rand_mag (180+rand()%(220-180+1)) //генерит для магазина от 180 до 220
 #define rand_gruz (150+rand()%(220-150+1)) //генерит для погрузчика от 150 до 220
-
+#define MAG_NUMBER 5 //колличесвто магаззинов 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct data
+{
+	pthread_mutex_t	mutex[MAG_NUMBER];
+	int	products[MAG_NUMBER];
+} shared;
 // static int pogruz;
-static int flag_mag1=0,flag_mag2=0,flag_mag3=0,flag_mag4=0,flag_mag5=0;
+//int flag_mag1=0,flag_mag2=0,flag_mag3=0,flag_mag4=0,flag_mag5=0;
 void* pokypatel1(void *args);
 void* pokypatel2(void *args);
-
+void* pokypatel3(void *args);
+void* pogryz(void *args);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(void){
 	pthread_t thread1;
     pthread_t thread2;
+    pthread_t thread3;
+    pthread_t thread4;
     srand(time(NULL));
-    int  m[5]={500,500,500,500,500};
-    
     int  pogruz=rand_gruz;
-    int mag[5];
-        for(int e=0;e<=5;e++ ){
-            mag[e]=m[e];
+    for (int i=0; i<MAG_NUMBER; i++)
+    {
+        shared.products[i]=rand_mag;
+    }
 
-        }
+   for(int a=0; a<MAG_NUMBER;a++)
+   {   
+       if(a==0){ printf("starting quantity of goods in the store\n"); } 
+       printf("|M%d=%d|",a+1,shared.products[a]);
 
-    printf(" MAGAZIN\n");   
-    printf(" ____________________\n");
-    printf("|%d||%d|%d|%d|%d|\n",m[0],m[1],m[2],m[3],m[4]);
-    printf(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
+       if(a==MAG_NUMBER-1) {printf("\nM-magazin and № №\n"); }
+    }
    
-   // printf("pogruzchik=%d\n",pogruz);
+    pthread_create(&thread1,NULL,pokypatel1,NULL);
+    pthread_create(&thread2,NULL,pokypatel2,NULL);
+    pthread_create(&thread3,NULL,pokypatel3,NULL);
+    pthread_create(&thread4,NULL,pogryz,NULL);
+         int a=pthread_join(thread1,NULL);
+        // printf("a iz main %d\n",a);
+         int b=pthread_join(thread2,NULL);
+       //  printf("b iz main %d\n",b);
+         int c=pthread_join(thread3,NULL);
+      //   printf("c iz main %d\n",b);
 
-    pthread_create(&thread1,NULL,pokypatel1,&mag[0]);
-         pthread_join(thread1,NULL);
-   // pthread_cancel(thread1);
-  //  pthread_create(&thread2,NULL,pokypatel2,NULL);
-      //   pthread_join(thread2,NULL);
+    if ((a&&b&&c)==0)  ///завершаем погрузчик
+    { 
+        pthread_cancel (thread4); 
+        printf("pogryzchik ymer\n"); 
+    }
+    
     
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////// покупаткль 1
 void* pokypatel1(void *args){ 
+     int hotelka=rand_pok;
+     printf("pokypatel 1 start=%d \n",hotelka);
+  //  int *i =(int*)args;
+    srand(time(NULL));
+    
+    while(1)
+    {
+        for (int i=0; i<MAG_NUMBER; i++) 
+        {
+            if(pthread_mutex_trylock(&shared.mutex[i])==0)
+            for(int a=0; a<MAG_NUMBER;a++)
+            {   
+                if (a==0){printf("pokepatel 1 zabral"); }
+                printf("|M%d=%d|",a+1,shared.products[a]);
+                
+                if(a==MAG_NUMBER-1) {printf("\n"); }
+            }
+            hotelka-=shared.products[i];
+            shared.products[i]=0;
+            
+            if(hotelka<0) { shared.products[i]=hotelka*-1; hotelka=0; }
+            printf("pokypatel 1 =%d \n",hotelka);
+            
+            pthread_mutex_unlock(&shared.mutex[i]);
 
-    int *i =(int*)args;
+            sleep(1);
+            if(hotelka==0)
+            {
+                 printf("potok1 ymer\n"); break; pthread_exit(NULL);
+                
+            }
 
+        }
+        if(hotelka==0) pthread_exit(NULL);
+        
+        
+    }  
+
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////// покупаткль 2
+void* pokypatel2(void *args){ 
+    // int *i =(int*)args;
+     int hotelka=rand_pok;
+     printf("pokypatel 2 start=%d \n",hotelka);
+     srand(time(NULL));
+
+
+  
+    while(1)
+    {
+        for (int i=0; i<MAG_NUMBER; i++) 
+        {
+            if(pthread_mutex_trylock(&shared.mutex[i])==0)
+            for(int a=0; a<MAG_NUMBER;a++)
+            {   
+                if (a==0){printf("pokepatel 2 zabral"); }
+                printf("|M%d=%d|",a+1,shared.products[a]);
+                
+                if(a==MAG_NUMBER-1) {printf("\n"); }
+            }
+            hotelka-=shared.products[i];
+            shared.products[i]=0;
+            
+            if(hotelka<0) { shared.products[i]=hotelka*-1; hotelka=0; }
+            printf("pokypatel 2 =%d \n",hotelka);
+            
+            pthread_mutex_unlock(&shared.mutex[i]);
+
+            sleep(1);
+            if(hotelka==0)
+            {
+                 printf("potok2 ymer\n"); break; pthread_exit(NULL);
+                
+            }
+
+        }
+        if(hotelka==0) pthread_exit(NULL);
+        
+        
+    }
+  
+ 
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////// покупатель 3
+void* pokypatel3(void *args){ 
+    // int *i =(int*)args;
+     int hotelka=rand_pok;
+     printf("pokypatel 3 start=%d \n",hotelka);
+     srand(time(NULL));
+
+    while(1)
+    {
+        for (int i=0; i<MAG_NUMBER; i++) 
+        {
+            if(pthread_mutex_trylock(&shared.mutex[i])==0)
+            for(int a=0; a<MAG_NUMBER;a++)
+            {   
+                if (a==0){printf("pokepatel 3 zabral"); }
+                printf("|M%d=%d|",a+1,shared.products[a]);
+                
+                if(a==MAG_NUMBER-1) {printf("\n"); }
+            }
+            hotelka-=shared.products[i];
+            shared.products[i]=0;
+            
+            if(hotelka<0) { shared.products[i]=hotelka*-1; hotelka=0; }
+            printf("pokypatel 3 =%d \n",hotelka);
+            
+            pthread_mutex_unlock(&shared.mutex[i]);
+
+            sleep(1);
+            if(hotelka==0)
+            {
+             printf("potok3 ymer\n"); break; pthread_exit(NULL);
+                
+            }
+
+        }
+        if(hotelka==0) pthread_exit(NULL);
+        
+        
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////// погрузчик
+void* pogryz(void *args){ 
+    
+    printf("POGRYZCHIK start\n");
+   // int *i =(int*)args;
+     int zagryzka=rand_gruz;
     srand(time(NULL));
 
-    int hotelka = rand_pok;
-    hotelka-=*i;
-    printf("pokypatel1=%d\n",hotelka);
-    printf("i=%d\n",*i);
+
   
-        while(hotelka!=0){
-            if(flag_mag1==0){
-                flag_mag1=1;
-                printf("pokypatel 1 v mag 1");
-                hotelka-=*i;
+        while(1){
+            for (int i=0; i<MAG_NUMBER; i++) 
+            {
+                if(pthread_mutex_trylock(&shared.mutex[i])==0)
+                for(int a=0; a<MAG_NUMBER;a++)
+                {   
+                 if (a==0){printf("POGRYZCHIK zagryzil"); }
+                 printf("|M%d=%d|",a+1,shared.products[a]);
+                 if(a==MAG_NUMBER-1) {printf("\n"); }
+                }
+                if (shared.products[i]==0)
+                {
+                shared.products[i]=zagryzka;
+                zagryzka=rand_gruz;
+                }
+               sleep(1);
+
             }
-            if(flag_mag2==0){
-                flag_mag2=1;
-                printf("pokypatel 1 v mag 2");
-                hotelka-=*(i+1);
-            }
-            if(flag_mag3==0){
-                flag_mag3=1;
-                printf("pokypatel 1 v mag 3");
-                hotelka-=*(i+2);
-            }
-            if(flag_mag4==0){
-                flag_mag4=1;
-                printf("pokypatel 1 v mag 4");
-                hotelka-=*(i+3);
-            }
-            if(flag_mag5==0){
-                flag_mag5=1;
-                printf("pokypatel 1 v mag 5");
-                hotelka-=*(i+4);
-            }
-            
+          
+           
         }
-  
-    if(hotelka==0){  printf("zakypilsa"); } 
-   // pthread_exit(0);pthread_testcancel();
-    
-}
-
-// void* pokypatel2(void *args){ 
-
-// }
+}        
